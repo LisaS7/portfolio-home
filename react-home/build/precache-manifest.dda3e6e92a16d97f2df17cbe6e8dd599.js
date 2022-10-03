fa8af439615ffa2d93ee1249df3f1d82e966230b
forest <mclisa00@gmail.com>
2022-10-03 14:52:18 +0100

    added build

---
+++ react-home/build/precache-manifest.dda3e6e92a16d97f2df17cbe6e8dd599.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "962c529c63fa2b6b5baf",
+    "url": "/static/css/main.d07023b5.chunk.css"
+  },
+  {
+    "revision": "962c529c63fa2b6b5baf",
+    "url": "/static/js/main.962c529c.chunk.js"
+  },
+  {
+    "revision": "96c881bc5d110cd660f0",
+    "url": "/static/js/1.96c881bc.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ede49eb32646cdaf96d957abbeb610c1",
+    "url": "/index.html"
+  }
+];
