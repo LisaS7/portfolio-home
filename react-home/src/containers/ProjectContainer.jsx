3422978b39ed03d600594204e0bccc8e8c3f23e2
Lisa <mclisa00@gmail.com>
2023-03-30 21:08:13 +0100

    Content for Mind Blank project page

---
+++ react-home/src/containers/ProjectContainer.jsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const ProjectContainer = styled.main`
+  margin: 4rem auto;
+  display: grid;
+  grid-template-columns: 49vw 49vw;
+  gap: 2vh 2vw;
+`;
--- react-home/src/pages/Projects/MindBlank.jsx
+++ react-home/src/pages/Projects/MindBlank.jsx
@@ -1,10 +1,52 @@
 import YouTube from "react-youtube";
+import styled from "styled-components";
+import { ProjectContainer } from "../../containers/ProjectContainer";
+
+const Image = styled.img`
+  margin: 4rem 0;
+  width: 80vw;
+`;
+
+const Textblock = styled.p`
+  margin: auto;
+  width: 80%;
+  max-width: 50vw;
+  text-align: justify;
+`;
+
+const ProjectSummary = styled.div`
+  text-align: left;
+`;
+
+const Row = styled.div`
+  display: flex;
+  align-items: center;
+`;
 
 export default function MindBlank() {
   return (
     <>
       <h1>Mind Blank</h1>
-      <YouTube videoId="hahfdp1couo" />
+      <ProjectContainer>
+        <YouTube videoId="hahfdp1couo" />
+        <ProjectSummary>
+          <p>Project Type: Group (4 developers)</p>
+          <p>Technologies: MERN stack, framer-motion</p>
+          <p>
+            Mind Blank was built as a group project during my time with
+            CodeClan. We worked in a team of 4 over the course of one week and
+            we presented our project to the group on the final day. We used
+            Agile practices such as a Kanban board and daily stand ups.
+          </p>
+        </ProjectSummary>
+      </ProjectContainer>
+      <Textblock>
+        We planned the app with wireframes, a components diagram, and user
+        stories. Our planning session was short and efficient and we all came
+        away with a clear idea of what we wanted to create.
+      </Textblock>
+      <Image src="../mind-blank/planning.png" alt="project user stories" />
+      <ProjectContainer></ProjectContainer>
     </>
   );
 }
